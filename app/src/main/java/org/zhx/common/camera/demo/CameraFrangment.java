f3cd164edd0b3e02bc3c2662e34a39f5ce711bed
zhouxue <194093798@qq.com>
2021-12-19 14:02:23 +0800

    fix scale bugs

--- app/src/main/java/org/zhx/common/camera/demo/CameraFrangment.java
+++ app/src/main/java/org/zhx/common/camera/demo/CameraFrangment.java
@@ -271,7 +271,7 @@ public class CameraFrangment extends BaseFragment implements View.OnTouchListene
         for (int i = 0; i < sizes.size(); i++) {
             Camera.Size previewSize = sizes.get(i);
             // 找到一个与设置的分辨率差值最小的相机支持的分辨率大小
-            ZCameraLog.e("getSuitableSize, width:" + previewSize.width + ", height:" + previewSize.height);
+//            ZCameraLog.e("getSuitableSize, width:" + previewSize.width + ", height:" + previewSize.height);
             if (previewSize.width * mRatio.getHeightRatio() / mRatio.getWidthRatio() == previewSize.height) {
                 int delta = Math.abs(screenP.x - previewSize.height);
                 if (minDelta >= delta) {
