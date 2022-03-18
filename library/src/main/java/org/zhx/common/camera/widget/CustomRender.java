941deceb6047113069ae0327ae19179653874fdb
zhouxue <194093798@qq.com>
2022-03-19 00:35:01 +0800

    rollback code

--- library/src/main/java/org/zhx/common/camera/widget/CustomRender.java
+++ library/src/main/java/org/zhx/common/camera/widget/CustomRender.java
@@ -94,7 +94,7 @@ public class CustomRender extends BaseRender {
     }
 
     public void onInit() {
-        glProgId = loadProgram(vertexShader, fragmentShader);
+        glProgId = GLHelper.loadProgram(vertexShader, fragmentShader);
         glAttribPosition = GLES20.glGetAttribLocation(glProgId, "position");
         glUniformTexture = GLES20.glGetUniformLocation(glProgId, "inputImageTexture");
         glAttribTextureCoordinate = GLES20.glGetAttribLocation(glProgId, "inputTextureCoordinate");
@@ -111,53 +111,6 @@ public class CustomRender extends BaseRender {
     public void setRotation(final Rotation rotation, boolean isFrontCamera) {
         setRotation(rotation, isFrontCamera, false);
     }
-
-    public static int loadProgram(final String strVSource, final String strFSource) {
-        int iVShader;
-        int iFShader;
-        int iProgId;
-        int[] link = new int[1];
-        iVShader = loadShader(strVSource, GLES20.GL_VERTEX_SHADER);
-        if (iVShader == 0) {
-            Log.d("Load Program", "Vertex Shader Failed");
-            return 0;
-        }
-        iFShader = loadShader(strFSource, GLES20.GL_FRAGMENT_SHADER);
-        if (iFShader == 0) {
-            Log.d("Load Program", "Fragment Shader Failed");
-            return 0;
-        }
-
-        iProgId = GLES20.glCreateProgram();
-
-        GLES20.glAttachShader(iProgId, iVShader);
-        GLES20.glAttachShader(iProgId, iFShader);
-
-        GLES20.glLinkProgram(iProgId);
-
-        GLES20.glGetProgramiv(iProgId, GLES20.GL_LINK_STATUS, link, 0);
-        if (link[0] <= 0) {
-            Log.d("Load Program", "Linking Failed");
-            return 0;
-        }
-        GLES20.glDeleteShader(iVShader);
-        GLES20.glDeleteShader(iFShader);
-        return iProgId;
-    }
-
-    public static int loadShader(final String strSource, final int iType) {
-        int[] compiled = new int[1];
-        int iShader = GLES20.glCreateShader(iType);
-        GLES20.glShaderSource(iShader, strSource);
-        GLES20.glCompileShader(iShader);
-        GLES20.glGetShaderiv(iShader, GLES20.GL_COMPILE_STATUS, compiled, 0);
-        if (compiled[0] == 0) {
-            Log.d("Load Shader Failed", "Compilation\n" + GLES20.glGetShaderInfoLog(iShader));
-            return 0;
-        }
-        return iShader;
-    }
-
 
     public void ifNeedInit() {
         if (!isInitialized) {
@@ -265,35 +218,10 @@ public class CustomRender extends BaseRender {
         if (runOnDraw.isEmpty()) {
             runOnDraw(() -> {
                 YUVTorgb.YUVtoRBGA(yuvData, width, height, glRgbBuffer.array());
-                glTextureId = createTextureIfNeeded(glRgbBuffer, width, height, glTextureId);
+                glTextureId = GLHelper.createTextureIfNeeded(glRgbBuffer, width, height, glTextureId);
             });
         }
     }
-
-    public static int createTextureIfNeeded(final IntBuffer data, final int width, final int height, final int usedTexId) {
-        int textures[] = new int[1];
-        if (usedTexId == -1) {
-            GLES20.glGenTextures(1, textures, 0);
-            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textures[0]);
-            GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
-                    GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
-            GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
-                    GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
-            GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
-                    GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE);
-            GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
-                    GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE);
-            GLES20.glTexImage2D(GLES20.GL_TEXTURE_2D, 0, GLES20.GL_RGBA, width, height,
-                    0, GLES20.GL_RGBA, GLES20.GL_UNSIGNED_BYTE, data);
-        } else {
-            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, usedTexId);
-            GLES20.glTexSubImage2D(GLES20.GL_TEXTURE_2D, 0, 0, 0, width,
-                    height, GLES20.GL_RGBA, GLES20.GL_UNSIGNED_BYTE, data);
-            textures[0] = usedTexId;
-        }
-        return textures[0];
-    }
-
 
     protected void runOnDraw(final Runnable runnable) {
         synchronized (runOnDraw) {
---
+++ library/src/main/java/org/zhx/common/camera/widget/GLHelper.java
@@ -0,0 +1,79 @@
+package org.zhx.common.camera.widget;
+
+import android.opengl.GLES20;
+import android.util.Log;
+
+import java.nio.IntBuffer;
+
+public class GLHelper {
+    public static int loadShader(final String strSource, final int iType) {
+        int[] compiled = new int[1];
+        int iShader = GLES20.glCreateShader(iType);
+        GLES20.glShaderSource(iShader, strSource);
+        GLES20.glCompileShader(iShader);
+        GLES20.glGetShaderiv(iShader, GLES20.GL_COMPILE_STATUS, compiled, 0);
+        if (compiled[0] == 0) {
+            Log.d("Load Shader Failed", "Compilation\n" + GLES20.glGetShaderInfoLog(iShader));
+            return 0;
+        }
+        return iShader;
+    }
+
+    public static int createTextureIfNeeded(final IntBuffer data, final int width, final int height, final int usedTexId) {
+        int textures[] = new int[1];
+        if (usedTexId == -1) {
+            GLES20.glGenTextures(1, textures, 0);
+            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textures[0]);
+            GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
+                    GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+            GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
+                    GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
+            GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
+                    GLES20.GL_TEXTURE_WRAP_S, GLES20.GL_CLAMP_TO_EDGE);
+            GLES20.glTexParameterf(GLES20.GL_TEXTURE_2D,
+                    GLES20.GL_TEXTURE_WRAP_T, GLES20.GL_CLAMP_TO_EDGE);
+            GLES20.glTexImage2D(GLES20.GL_TEXTURE_2D, 0, GLES20.GL_RGBA, width, height,
+                    0, GLES20.GL_RGBA, GLES20.GL_UNSIGNED_BYTE, data);
+        } else {
+            GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, usedTexId);
+            GLES20.glTexSubImage2D(GLES20.GL_TEXTURE_2D, 0, 0, 0, width,
+                    height, GLES20.GL_RGBA, GLES20.GL_UNSIGNED_BYTE, data);
+            textures[0] = usedTexId;
+        }
+        return textures[0];
+    }
+
+    public static int loadProgram(final String strVSource, final String strFSource) {
+        int iVShader;
+        int iFShader;
+        int iProgId;
+        int[] link = new int[1];
+        iVShader = GLHelper.loadShader(strVSource, GLES20.GL_VERTEX_SHADER);
+        if (iVShader == 0) {
+            Log.d("Load Program", "Vertex Shader Failed");
+            return 0;
+        }
+
+        iFShader = GLHelper.loadShader(strFSource, GLES20.GL_FRAGMENT_SHADER);
+        if (iFShader == 0) {
+            Log.d("Load Program", "Fragment Shader Failed");
+            return 0;
+        }
+
+        iProgId = GLES20.glCreateProgram();
+
+        GLES20.glAttachShader(iProgId, iVShader);
+        GLES20.glAttachShader(iProgId, iFShader);
+
+        GLES20.glLinkProgram(iProgId);
+
+        GLES20.glGetProgramiv(iProgId, GLES20.GL_LINK_STATUS, link, 0);
+        if (link[0] <= 0) {
+            Log.d("Load Program", "Linking Failed");
+            return 0;
+        }
+        GLES20.glDeleteShader(iVShader);
+        GLES20.glDeleteShader(iFShader);
+        return iProgId;
+    }
+}
